00b02c58f8b27b403d5d1d75302238154d0f5311
Shiraeda <ASOPC>
2020-11-06 15:40:48 +0900

    敵の視界内サーチとナビメッシュの仮移動を実装しました。

--- Isometric3D/Assets/Scripts/Debug/DebugGizmos.cs
+++ Isometric3D/Assets/Scripts/Debug/DebugGizmos.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class DebugGizmos : MonoBehaviour
 {
+    private enum TYPE
+	{
+        CUBE,
+        SPHERE,
+        MAX
+	}
+
+    [SerializeField, Tooltip("")]
+    private TYPE _type = TYPE.CUBE;
     [SerializeField, Tooltip("ギズモの色指定")]
     private Color _color = Color.white;
+    [SerializeField]
+    private bool _gameMode;
+
+
+    private MeshFilter _meshFilter;
+    private MeshRenderer _meshRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -23,6 +38,17 @@ public class DebugGizmos : MonoBehaviour
         Gizmos.color = _color;
         Matrix4x4 matrix = transform.localToWorldMatrix;
         Gizmos.matrix = matrix;
-        Gizmos.DrawCube(Vector3.zero, Vector3.one);
+		switch (_type)
+		{
+			case TYPE.CUBE:
+				Gizmos.DrawCube(Vector3.zero, Vector3.one);
+				break;
+			case TYPE.SPHERE:
+                Gizmos.DrawSphere(Vector3.zero, 0.5f);
+                break;
+			case TYPE.MAX:
+			default:
+				break;
+		}
 	}
 }
---
+++ Isometric3D/Assets/Scripts/EnemySearch.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.AI;
+
+[ExecuteInEditMode]
+public class EnemySearch : MonoBehaviour
+{
+    [SerializeField]
+    private Color _color = Color.white;
+    [SerializeField]
+    private float _distans = 0.5f;
+    [SerializeField]
+    private float _searchAngle = 60f;
+    private SphereCollider _sphere;
+    private NavMeshAgent _agent;
+    private GameObject _target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _sphere = GetComponent<SphereCollider>();
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+	private void OnTriggerStay(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+            Debug.Log("当たり判定に入っている");
+            var playerDirection = other.transform.position - transform.position;
+            var angle = Vector3.Angle(transform.forward, playerDirection);
+            if(angle <= _searchAngle)
+			{
+                Debug.Log("プレイヤーの発見");
+                _agent.isStopped = false;
+                Debug.Log(_agent.isStopped);
+                _agent.SetDestination(other.transform.position);
+			}
+            else
+			{
+                Debug.Log("範囲外に移動");
+                _agent.isStopped = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("範囲外に移動");
+            _agent.isStopped = true;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Handles.color = _color;
+        Handles.DrawSolidArc(transform.position, Vector3.up, Quaternion.Euler(0f, -_searchAngle, 0f) * transform.forward, _searchAngle * 2f, _sphere.radius);
+    }
+}
