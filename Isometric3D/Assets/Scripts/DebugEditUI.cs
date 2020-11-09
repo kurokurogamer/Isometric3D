ff3774dcd44ee8f4f5f076e3ee69ced6a478d793
Shiraeda <ASOPC>
2020-11-09 15:32:03 +0900

    コミット

---
+++ Isometric3D/Assets/Scripts/DebugEditUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugEditUI : MonoBehaviour
+{
+    private Dictionary<TestItemBase, int> _itemList;
+    private Dictionary<int, int> _itemDate;
+    private bool _debugMode;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public void NnmPuls(TestItemBase baseItem, int num)
+    {
+        if(_itemDate.ContainsKey(baseItem.ItemID))
+		{
+            _itemDate.Add(baseItem.ItemID, 0);
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(_debugMode)
+		{
+            gameObject.SetActive(true);
+		}
+        else
+		{
+            gameObject.SetActive(false);
+		}
+    }
+}
---
+++ Isometric3D/Assets/Scripts/TestItemBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestItemBase : ScriptableObject
+{
+	int _itemId;
+	public int ItemID
+	{
+		get
+		{
+			return _itemId;
+		}
+	}
+
+}
